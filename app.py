2a05c1f905cc577e8639b23594f60eba4828d734
djason <djason@cougarnet.uh.edu>
2024-11-02 22:29:28 -0500

    Initial Commit - Some functionality (BARE BONES)

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+def grab_ip():
+    response = requests.get("https://api.ipify.org/")
+    if response.status_code == 200:
+        public_ip = response.text.strip()
+    else:
+        public_ip = "Error retrieving IP"
+    return public_ip
+
+def get_location(ip_address):
+    response = requests.get(f"https://ipinfo.io/{ip_address}/json")
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        return None
+
+ip_address = grab_ip()
+location_data = get_location(ip_address)
+
+@app.route('/')
+def index():
+    response = requests.get("https://api.ipify.org/")
+    if response.status_code == 200:
+        public_ip = response.text.strip()
+    else:
+        public_ip = "Error retrieving IP"
+    return render_template('index.html', ip_address=public_ip, country=location_data['country'], region=location_data['region'], city=location_data['city'])
+
+@app.route('/', methods=['POST'])
+def post_city():
+    if request.method == 'POST':
+        city = request.form.get('city')
+        print(city)
+        if location_data and 'city' in location_data:
+            user_city = city.upper()
+            ip_city = location_data['city'].upper()
+            if user_city == ip_city:
+                return render_template('success.html')
+            else:
+                return render_template('failed.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
